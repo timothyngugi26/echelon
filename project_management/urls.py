30a915712cdfa59d4cc6f27bcc16ee9ebc19adfb
Pury-01 <puryokumu@gmail.com>
2024-09-05 15:35:48 +0300

    landing page designing and styling

--- project_management/urls.py
+++ project_management/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns  = [
 	path('', views.index, name='index'), #demo route
+        path('login/', views.login_view, name='login'),
+        path('signup/', views.signup_view, name='signup'),
         path('tasks/', views.task_list, name='task_list'),
 ]
--- project_management/views.py
+++ project_management/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import AuthenticationForm
 
 def index(request):
-    return render(request, 'project_management/index.html')
+    form = AuthenticationForm()
+    return render(request, 'project_management/index.html',  {'form': form})
+
+
+def login_view(request):
+    if request.method  == 'POST':
+        pass
+    return render(request, 'project_management/login.html')
+
+def signup_view(request):
+    if request.method == 'POST':
+        pass
+    return render(request, 'project_management/signup.html')
 
 def task_list(request):
     # Fetch tasks from the database (assuming a Task model exists)
