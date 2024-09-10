406e34150843e067d46290b1f162efbd90f37618
Pury-01 <puryokumu@gmail.com>
2024-09-10 12:36:58 +0300

    corrected paths and logic to fix login and signup errors

--- project_management/urls.py
+++ project_management/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from .views import signup_view
+from .views import profile_page
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
@@ -10,12 +12,12 @@ urlpatterns  = [
 	path('', views.index, name='index'),
 
         # Authetication routes
-        path('signup/', views.signup_view, name='signup'),
+        path('signup/', signup_view, name='signup'),
         path('tasks/', views.task_list, name='task_list'),
         path('accounts/', include('django.contrib.auth.urls')),
         # The profile page 
 
-        path('profile/', views.profile_page, name='profile_page'),
+        path('profile/', profile_page, name='profile_page'),
         path('project-in-progress/', views.project_in_progress_page, name='project_in_progress'),
         path('projects/', views.projects_page, name='projects'),
 ]
--- project_management/views.py
+++ project_management/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth import authenticate, login as auth_login
 from django.shortcuts import render
+from django.http import HttpResponse
 
 def profile_page(request):
     return render(request, 'profile_page.html')
@@ -29,18 +30,21 @@ def login_view(request):
         return render(request, 'registration/login.html', {'form': form})
 
 def signup_view(request):
-    print("Entering signup_view")
+    # print("Entering signup_view")
+    # return HttpResponse("The view works!")
     if request.method == 'POST':
-        form == UserCreationForm(request.POST)
+        form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
             auth_login(request, user)
-            print("User signed up and logged in")
+            # print("User signed up and logged in")
             return redirect('profile_page')
         else:
-            form = UserCreationForm()
+            print('Invalid form')
+    else:
+        form = UserCreationForm()
         
-        return render(request, 'project_management/signup.html', {'form': form})
+    return render(request, 'project_management/signup.html', {'form': form})
 
 def task_list(request):
     # Fetch tasks from the database (assuming a Task model exists)
@@ -48,10 +52,10 @@ def task_list(request):
     return render(request, 'project_management/task_list.html', {'tasks': tasks})
 
 def profile_page(request):
-    return render(request, 'profile_page.html')
+    return render(request, 'project_management/profile_page.html')
 
 def projects_page(request):
-    return render(request, 'projects.html')
+    return render(request, 'project_management/projects.html')
 
 def project_in_progress_page(request):
-    return render(request, 'project_in_progress.html')
+    return render(request, 'project_management/project_in_progress.html')
